1723a6eea7bc48b5450d042a38f0ad81aa2f368a
jjohn92085 <jjohn92085@gmail.com>
2026-04-22 23:18:43 -0700

    first commit

--- Service-Health/service-check-tool.ps1
+++ Service-Health/service-check-tool.ps1
@@ -1,16 +1,21 @@
 
 # this is a tool to check if specific services are running on startup and turn them off as needed
 
-#run in elevation
+# run as admin
 
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
 $geolocation = (Get-Service -Name "Geolocation Service").status
 
-if ($geolocation -eq "Running" ) {
+#check if service is  running and stop it
+
+if ($geolocation -eq "Running" -or "StartPending" -or "ContinuePending" ) {
 
     Stop-Service -Name "Geolocation Service"
 
+} 
+else {
+
+    Write-Host "The Geolocation service is stopped"
 }
 
-elseif ($geolocation)
